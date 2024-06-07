79ce05e6d9c2ee361ce98332b5a7fbe6457275df
Rafli Ramadhan <rafliramdhn@gmail.com>
2024-06-07 15:15:59 +0700

    menambahkan upload feature pada context and question

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import gradio as gr
 import pandas as pd
 import numpy as np
-import fitz
+import fitz #PyMuPDF
 from openai import OpenAI
 
 client = OpenAI(base_url="http://localhost:1234/v1", api_key="lm-studio")
@@ -47,21 +47,6 @@ def read_pdf(file):
         history.append({"role": "user", "content": text})
     return text
 
-
-def get_dataframe():
-    data = {
-        "Konteks": ["Konteks 1", "Konteks 2", "Konteks 3", "Konteks 4", "Konteks 5"],
-    }
-    df = pd.DataFrame(data)
-    return df
-
-def get_dataframe2():
-    data = {
-        "Item": ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"],
-    }
-    df = pd.DataFrame(data)
-    return df
-
 # Fungsi chatbot sederhana
 def chatbot(input_text):
     responses = {
@@ -83,8 +68,8 @@ def chatbot2(input_text):
 
 chatbot_component2 = gr.Chatbot(label="Catatan Response model")
 
-data = get_dataframe()
-data2 = get_dataframe2()
+data  = pd.DataFrame()
+data2 = pd.DataFrame()
 
 def chatbot_response(history, input_text):
     response = chatbot(input_text)
@@ -100,33 +85,47 @@ def place(evt: gr.SelectData):
     return data['Konteks'].values[evt.index[0]]
 
 def place2(evt: gr.SelectData):
-    return data2['Item'].values[evt.index[0]]
+    return data2['Pertanyaan'].values[evt.index[0]]
 
-# Layout Gradio
+def read_kontek(file):
+    df = pd.read_csv(file.name)
+    data["Konteks"] = df["Konteks"]
+    return df
+
+def read_pertanyaan(file):
+    df = pd.read_csv(file.name)
+    data2["Pertanyaan"] = df["Pertanyaan"]
+    return df
+
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column(scale=1):
-            df_input = gr.DataFrame(data, label="Daftar Konteks")
-            text_output = gr.Textbox(show_label=False)
-            df_input.select(place, None, text_output)
+            with gr.Row():
+                    df_input = gr.DataFrame(data, label="Daftar Konteks", headers=["Konteks"], datatype=['str'], row_count=5, col_count=(1, "fixed"))
+                    text_output = gr.Textbox(show_label=False)
+                    df_input.select(place, None, text_output)
+            with gr.Row():
+                    pdf_konteks = gr.File(label="Unggah Dokumen Konteks", type="filepath")
+                    pdf_konteks.change(read_kontek, pdf_konteks, df_input)
 
         with gr.Column(scale=1):
-            df_input2 = gr.DataFrame(data2, label="Standar Pertanyaan")
-            text_output2 = gr.Textbox(show_label=False)
-            df_input2.select(place2, None, text_output2)
+            with gr.Row():
+                df_input2 = gr.DataFrame(data2, label="Standar Pertanyaan", headers=["Pertanyaan"], datatype=['str'], row_count=5, col_count=(1, "fixed"))
+                text_output2 = gr.Textbox(show_label=False)
+                df_input2.select(place2, None, text_output2)
+            with gr.Row():
+                pdf_pertanyaan = gr.File(label="Unggah Dokumen Pertanyaan", type="filepath")
+                pdf_pertanyaan.change(read_pertanyaan, pdf_pertanyaan, df_input2)
 
         with gr.Column(scale=2):
             gr.ChatInterface(fn=converse)
         with gr.Column(scale=1):
             chatbot_component2.render()
 
-    with gr.Row():
+        with gr.Row():
             pdf_input = gr.File(label="Unggah PDF", type="filepath")
             pdf_output = gr.Textbox(label="Isi PDF")
             pdf_input.change(read_pdf, pdf_input, pdf_output)
-
-  
-        
 
 # Jalankan Gradio
 demo.launch()
